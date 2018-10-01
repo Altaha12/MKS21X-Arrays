1aa6205cf52aa084180f810b9f5fdcfc0013cbb2
Ali Taha <ataha1@stuy.edu>
2018-10-01 09:36:18 -0400

    problem 1 finished

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -15,11 +15,20 @@ public static void printArray(int[][]ary){
   }
   System.out.println("]");
 }
+public static int countZeros2D(int[][] nums){
+  int numzeros=0;
+  for(int i=0;i<nums.length;i++){
+    for(int k=0; k<nums[i].length;k++){
+      if (nums[i][k]==0)numzeros+=1;}  
+  }
+  return numzeros;
+}
 public static void main(String[] args){
   int[]x= new int[3];
   x[0]=1;
   int[][]y=new int[2][2];
   y[0][1]=3;
  printArray(x);
- printArray(y);}
-}
+ printArray(y);
+ System.out.println(countZeros2D(y));
+}}
