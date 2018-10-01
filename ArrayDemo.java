fee61910966058156f48de331bdf2eb60ff1e74c
Ali Taha <ataha1@stuy.edu>
2018-10-01 10:21:18 -0400

    problem 2b finished

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,5 +1,5 @@
 public class ArrayDemo{
-public static void printArray(int[]ary){
+public static void printArray(int[]ary)/*0a*/{
   String r = "[";
   for(int i=0;i<ary.length;i++){
     r+=ary[i];
@@ -8,14 +8,14 @@ public static void printArray(int[]ary){
   r+="]";
   System.out.println(r);
 }
-public static void printArray(int[][]ary){
+public static void printArray(int[][]ary)/*0b*/{
   System.out.println("[");
   for(int i=0;i<ary.length;i++){
     printArray(ary[i]);
   }
   System.out.println("]");
 }
-public static int countZeros2D(int[][] nums){
+public static int countZeros2D(int[][] nums)/*1*/{
   int numzeros=0;
   for(int i=0;i<nums.length;i++){
     for(int k=0; k<nums[i].length;k++){
@@ -23,7 +23,7 @@ public static int countZeros2D(int[][] nums){
   }
   return numzeros;
 }
-public static void fill2D(int[][] vals){
+public static void fill2D(int[][] vals)/*2a*/{
   for(int i=0;i<vals.length;i++){
     for(int k=0;k<vals[i].length;k++){
       vals[i][k]=1;
@@ -31,7 +31,16 @@ public static void fill2D(int[][] vals){
     }
   }
 }
-public static void main(String[] args){
+public static int[][] fill2DCopy(int[][] vals)/*2b*/{
+  int[][]valscopy= new int[vals.length][vals[0].length];
+  for(int i=0;i<vals.length;i++){
+    for(int k=0;k<vals[i].length;k++){
+      valscopy[i][k]=1;
+      if(vals[i][k]<0)valscopy[i][k]=3;
+    }}
+  return valscopy;
+  }
+public static void main(String[] args)/**/{
   int[]x= new int[3];
   x[0]=1;
   int[][]y=new int[2][2];
@@ -41,5 +50,7 @@ public static void main(String[] args){
  System.out.println(countZeros2D(y));
  fill2D(y);
  printArray(y);
+ y[0][0]=-4;
+ printArray(fill2DCopy(y));
  System.out.println(countZeros2D(y));
 }}
